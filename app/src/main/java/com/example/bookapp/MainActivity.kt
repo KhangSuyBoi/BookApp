9d09eaa49579f0b720084d42ae52f65d6d78f62a
KhangSuyBoi <143074332+KhangSuyBoi@users.noreply.github.com>
2024-05-14 14:45:51 +0700

    140524

---
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.bookapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.bookapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+}
